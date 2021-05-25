0de946f04aee30faaebaaa9c939e13acf86d7a1e
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2021-05-25 18:14:22 -0300

    Busca por repositórios de usuários

---
+++ desafio_processo_seletivo/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './pages/home/App.js';
+import BuscarUsuario from "./pages/buscarUsuario/buscarUsuario.js"
+import reportWebVitals from './reportWebVitals';
+
+import {Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/buscarusuarios" component={BuscarUsuario}/> 
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing,document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ desafio_processo_seletivo/src/pages/buscarUsuario/buscarUsuario.js
@@ -0,0 +1,85 @@
+import {Component} from 'react'
+
+
+
+class BuscarUsuario extends Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            listaRepositorios : [],
+            nomeUsuario : ''
+        }
+    }
+
+    componentDidMount(){
+    }
+
+    atualizarNomeUsuario = async (event) =>{
+        await this.setState({nomeUsuario : event.target.value})
+        console.log(this.state.nomeUsuario)
+    }
+
+    buscarUsuario = () => {
+        
+        fetch("https://api.github.com/users/"+this.state.nomeUsuario+"/repos")
+        .then(resposta => resposta.json())
+        .then(data => this.setState({listaRepositorios : data}))
+        .catch(erro => console.log(erro))
+    }
+
+    render(){
+        return(
+            <div>
+                <main>
+                    <h1>Buscar Repositórios por Usuários</h1>
+                    <section>
+                        <input
+                        type="text"
+                        value={this.state.nomeUsuario}
+                        onChange={this.atualizarNomeUsuario}
+                        placeholder="Nome do Usuário"
+                        />
+                        <button type="submit" onClick={this.buscarUsuario} disabled={this.state.nomeUsuario === "" ? 'none' : ''}>
+                            Buscar
+                        </button>
+                        <hr/>
+                    </section>
+
+                    
+                    <section>
+                        <h3>{this.state.nomeUsuario}</h3>
+                       <table>
+                           <thead>
+                               <tr>
+                                   <th>Id</th>
+                                   <th>Repositório</th>
+                                   <th>Descrição</th>
+                                   <th>Data de Criação</th>
+                                   <th>Tamanho</th>
+                               </tr>
+                           </thead>
+
+                           <tbody>
+                                {
+                                    this.state.listaRepositorios.map(tr => {
+                                        return(
+                                            <tr key={tr.id}>
+                                                <td>{tr.id}</td>
+                                                <td>{tr.name}</td>
+                                                <td>{tr.description}</td>
+                                                <td>{tr.created_at}</td>
+                                                <td>{tr.size}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                           </tbody>
+                       </table>
+                    </section>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default BuscarUsuario
